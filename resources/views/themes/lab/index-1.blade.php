d16df194a23dacf0ec4e07f361e1610852596c66
nosorrow <plamenorama@gmail.com>
2025-03-15 18:29:33 +0200

    2025-03-15

--- resources/views/themes/lab/index-1.blade.php
+++ resources/views/themes/lab/index-1.blade.php
@@ -341,11 +341,6 @@
 
 {{--Services Section--}}
 <section class="relative bg-white dark:bg-gray-900 mx-auto">
-    <div class="absolute top-0 right-0"
-         style="background-image: url('{{ url('images/shape-4.png') }}'); background-position: left; background-repeat: no-repeat;
-            width: 513px; height: 529px;
-        "
-    ></div>
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-md mb-8 lg:mb-16" data-aos="fade-up">
             <h2 class="text-center mb-4 text-4xl tracking-tight font-extrabold text-cyan-800 dark:text-white">
@@ -453,11 +448,11 @@
 
 {{--Contact Section--}}
 <section id="contact" class="relative bg-transparent dark:bg-gray-900">
-    <div class="absolute top-0 left-0"
-        style="background-image: url('{{ url('images/shape-3.png') }}'); background-position: left; background-repeat: no-repeat;
-            width: 513px; height: 529px; z-index: -1;
-        "
-    ></div>
+{{--    <div class="absolute top-0 left-0"--}}
+{{--        style="background-image: url('{{ url('images/shape-3.png') }}'); background-position: left; background-repeat: no-repeat;--}}
+{{--            width: 513px; height: 529px; z-index: -1;--}}
+{{--        "--}}
+{{--    ></div>--}}
     <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-cyan-800 dark:text-white">Contact Us</h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
