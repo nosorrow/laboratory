0969d560afae34befda4988ae22debac6faff076
nosorrow <plamenorama@gmail.com>
2025-03-15 18:58:23 +0200

    2025-03-15

--- resources/views/themes/lab/index-1.blade.php
+++ resources/views/themes/lab/index-1.blade.php
@@ -196,17 +196,18 @@
                 d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
             ></path>
         </svg>
-        <svg viewBox="0 0 52 24" fill="currentColor"
-             class="hidden opacity-30 absolute bottom-0 right-0 w-32 text-blue-gray-100 lg:w-32 sm:block"
-        >
-            <defs>
-                <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42363" x="0" y="0" width=".135" height=".30">
-                    <circle cx="1" cy="1" r=".7"></circle>
-                </pattern>
-            </defs>
-            <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)" width="52" height="24"></rect>
-        </svg>
-
+        <div class="hidden sm:block">
+            <svg viewBox="0 0 52 24" fill="currentColor"
+                 class="opacity-30 absolute bottom-0 right-0 w-32 text-blue-gray-100 lg:w-32 sm:block"
+            >
+                <defs>
+                    <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42364" x="0" y="0" width=".135" height=".30">
+                        <circle cx="1" cy="1" r=".7"></circle>
+                    </pattern>
+                </defs>
+                <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42364)" width="52" height="24"></rect>
+            </svg>
+        </div>
         <div class="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div class="flex flex-col items-center justify-between xl:flex-row">
                 <div class="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
