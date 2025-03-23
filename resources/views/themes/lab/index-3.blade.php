ec71789967673ba04b83f975dc824f5c74fd0573
nosorrow <plamenorama@gmail.com>
2025-03-23 22:16:29 +0200

    2025-03-23, Hero 3 mobile add divider

--- resources/views/themes/lab/index-3.blade.php
+++ resources/views/themes/lab/index-3.blade.php
@@ -84,7 +84,7 @@
         }
 
         .custom-shape-divider-bottom-1742753328 .shape-fill {
-            fill: #FFFFFF;
+            fill: #ffffff;
         }
 
         /*mobile divider*/
@@ -268,7 +268,7 @@
             </svg>
         </div>
 {{--        Mobile Divider--}}
-        <div class="custom-shape-divider-bottom-1742758230">
+        <div class="sm:hidden custom-shape-divider-bottom-1742758230">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
             </svg>
