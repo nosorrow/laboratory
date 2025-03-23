6f33cfe74e1519ba4bb9321b7e4804eb09a7291f
nosorrow <plamenorama@gmail.com>
2025-03-23 23:40:37 +0200

    2025-03-23, Content image

--- resources/views/themes/lab/index-3.blade.php
+++ resources/views/themes/lab/index-3.blade.php
@@ -375,9 +375,10 @@
 </div>
 
 {{--Content 1--}}
-<div class="-z-999 -mt-10 relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+<div class="-z-999 relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
     <div class="absolute inset-0 -z-10 overflow-hidden">
-        <svg class="absolute top-0 left-[max(50%,25rem)] h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+        <svg class="absolute top-0 left-[max(50%,25rem)] h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200
+         [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
             <defs>
                 <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
                     <path d="M100 200V.5M.5 .5H200" fill="none" />
@@ -395,7 +396,10 @@
                 <div class="lg:max-w-lg">
                     <p class="sub-heading text-base/7 font-semibold text-teal-600">Loren Ipsun</p>
                     <h1 class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-cyan-800 sm:text-5xl">A better workflow</h1>
-                    <p class="mt-6 text-xl/8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+                    <p class="mt-6 text-xl/8 text-gray-700">
+                        Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
+                        At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.
+                    </p>
                     <ul class="mt-6 space-y-2 text-cyan-800 font-bold">
                         <li><i class="fa-solid fa-circle-check mr-2 text-lg text-teal-500"></i>
                             Led by Passionate Experts</li>
@@ -404,23 +408,24 @@
                         <li><i class="fa-solid fa-circle-check mr-2 text-lg text-teal-500"></i>
                             Safe diagnoses and effective therapies</li>
                     </ul>
+                    <p class="mt-6 text-xl/8 text-gray-700">
+                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                        Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat.
+                        Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                    </p>
+
                 </div>
             </div>
         </div>
-        <div style="mask: linear-gradient(90deg, transparent, #54d9af 10%, #00655d 85%, transparent);"
-             class="-mt-12 -ml-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden relative">
-            <img class="w-100 max-w-none rounded-t-lx rounded-bl-[20%] bg-gray-900 ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem]"
-                 src="{{ url('images/01.jpg') }}" alt=""
-                 style="mask: linear-gradient(90deg, transparent, #54d9af 10%, #00655d 85%, transparent);"
+        <div
+{{--            style="mask: linear-gradient(90deg, transparent, #54d9af 10%, #00655d 85%, transparent);"--}}
+             class="-mt-12 -ml-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden relative"
+        >
+            <img class="w-100 max-w-none rounded-t-lx rounded-bl-[20%] bg-gray-900 ring-1 ring-gray-400/10 sm:w-[57rem]"
+                 src="{{ url('images/hero-v2-4.jpg') }}" alt=""
+                 style="mask: linear-gradient(180deg, transparent, #54d9af 10%, #00655d 85%, transparent);"
 
             >
-        </div>
-        <div class="lg:-mt-20 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-            <div class="lg:pr-4">
-                <div class="max-w-xl text-base/7 text-gray-700 lg:max-w-lg" data-aos="fade-up" data-aos-delay="200">
-                    <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
-                </div>
-            </div>
         </div>
     </div>
 </div>
