b2abaab9b2df2efee3f5c991988a47c4dade54fd
nosorrow <plamenorama@gmail.com>
2025-03-28 23:04:09 +0200

    2025-03-28, change mobile button color

--- resources/views/themes/lab/index-3.blade.php
+++ resources/views/themes/lab/index-3.blade.php
@@ -132,7 +132,7 @@
         </a>
         <button
                 data-collapse-toggle="mega-menu-full-cta"
-                type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-cyan-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-cyan-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full-cta" aria-expanded="false">
+                type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-cyan-800 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-cyan-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full-cta" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg id="hamburger-icon" class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
